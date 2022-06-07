076a6f0e181fb5fec7a31d624ff15e0f96a41575
abdisalamhassan <95972020+abdisalamhassan@users.noreply.github.com>
2022-06-07 17:40:34 -0400

    worked on javascript

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,5 +1,18 @@
-var userFormEl = document.querySelector("#user-form");
-var languageButtonsEl = document.querySelector("#city-buttons");
-var nameInputEl = document.querySelector("#cityname");
-var repoContainerEl = document.querySelector("#citiess-container");
-var repoSearchTerm = document.querySelector("#city-search-term");
+let weather = {
+    "apiKey": "d8b9c9f29acf75b9bc89cdc565bacf19",
+    fetchWeather: function(city){
+        fetch(
+            "https://api.openweathermap.org/data/2.5/weather?q=" 
+            + city 
+            +  "&units=metric&appid=" 
+            + this.apiKey
+            )
+            
+            .then((response) => response.json())
+            .then((data) => console.log(data));
+    },
+    displayWeather: function(data){
+        const{ name } = data;
+        const {icon,description} = data.weather
+    }
+}
---
+++ readme.md
@@ -0,0 +1,13 @@
+## User Story
+The purpose of this app is to help travellers figure out what the weather could be in any given city by simply typing in into a search bar
+
+## Technologies Used
+-HTML
+-CSS
+-Javascript
+
+## Screenshots
+
+
+## Created By
+Abdi Hassan
