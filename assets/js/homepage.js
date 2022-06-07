3672afe971e665752a9cda5a71788040eb66f5c5
abdisalamhassan <95972020+abdisalamhassan@users.noreply.github.com>
2022-06-07 16:19:54 -0400

    skeleton design

---
+++ assets/js/homepage.js
@@ -0,0 +1,5 @@
+var userFormEl = document.querySelector("#user-form");
+var languageButtonsEl = document.querySelector("#city-buttons");
+var nameInputEl = document.querySelector("#cityname");
+var repoContainerEl = document.querySelector("#citiess-container");
+var repoSearchTerm = document.querySelector("#city-search-term");
